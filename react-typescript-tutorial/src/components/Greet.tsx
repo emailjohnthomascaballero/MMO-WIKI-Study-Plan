c55579509f2fb071490138f057d8dbf3a00b776b
John <emailjohnthomascaballero@gmail.com>
2023-10-05 13:26:09 +0800

    Day 2

--- react-typescript-tutorial/src/components/Greet.tsx
+++ react-typescript-tutorial/src/components/Greet.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 type GreetProps = {
-  name: string
+  name: string;
+  messagecount: number;
+  isLoggedIn: boolean;
 }
 
 const Greet = (props: GreetProps) => {
   return (
-    <div><h2>Hello, {props.name}</h2></div>
-  )
-}
+    <div>
+      <h2>
+        {props.isLoggedIn 
+          ? `Hello, ${props.name}, your favorite number is ${props.messagecount}` 
+          : `Welcome Stranger`}
+      </h2>
+    </div>
+  );
+};
 
 export default Greet
